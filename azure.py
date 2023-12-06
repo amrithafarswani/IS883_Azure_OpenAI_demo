ab732fac53232609201e47825181168c59fd05b5
amrithafarswani <122059440+amrithafarswani@users.noreply.github.com>
2023-12-06 14:35:27 -0500

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,19 +1,39 @@
-# azure.py
-# Import necessary libraries
 import streamlit as st
+import pandas as pd
 import openai
 import os
+
+# Load your dataset
+file_path = "lyrics_dataset_all.csv"  # Update the path to your dataset
+df = pd.read_csv(file_path)
+
+# Filter the dataset to keep only the 'track_artist' and 'lyrics' columns
+artist_lyrics = df[['artist', 'lyrics']]
+
 # Set up your OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
-# Initialize Streamlit
-st.title("Azure Guide Chatbot")
-# Create a text input field for user queries
-user_input = st.text_input("Ask a question:")
-# Send the user's query to OpenAI GPT-3
-if user_input:
+
+# Streamlit app title
+st.title("Lyrics Generator")
+
+# Dropdown to select the artist
+selected_artist = st.selectbox('Select an artist:', artist_lyrics['artist'].unique())
+
+
+# Function to generate lyrics based on selected artist
+def generate_lyrics(artist):
+    artist_lyrics = df[df['artist'] == artist]['lyrics'].dropna().tolist()
+    combined_lyrics = '\n'.join(artist_lyrics[:5])  # Reduce the total lines to fit within context limits
+    combined_lyrics = combined_lyrics[:2048]  # Limit the total characters to 2048 to fit token limits
     response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=user_input,
-    max_tokens=50
+        model="text-davinci-003",
+        prompt=combined_lyrics,
+        max_tokens=100
     )
-    st.write(response['choices'][0]['text'].strip())
+    return response.choices[0].text.strip()
+
+# Generate and display lyrics based on the selected artist when the button is clicked
+if selected_artist:
+    if st.button('Generate Lyrics'):
+        generated_lyrics = generate_lyrics(selected_artist)
+        st.write(generated_lyrics)
