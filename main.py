f85d5122ad4cdffaac96a4a60f8999b6904b77ca
amrithafarswani <122059440+amrithafarswani@users.noreply.github.com>
2023-12-12 15:09:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ import os
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Function to generate lyrics
-def generate_lyrics(artist_name, genre, subject=None, rhyme=None, temperature=0.7, use_slang=False):
-    prompt = f"Imagine you are a songwriter. Write the lyrics to a song based on this {genre} that the author wants: {subject}, in similarity to this artist: {artist_name}, and if available create rhymes with this phrase {rhyme}. Try your best to match the style of the artist. Unless specified, do not use slang or casual langauge in the lyrics generated."
+def generate_lyrics(artist_name, genre, subject=None, rhyme=None, temperature=0.7, use_slang=False, include_non_lexical=False, specific_non_lexical=None):
+    prompt = f"Imagine you are a songwriter. Write the lyrics to a song based on this {genre} that the author wants: {subject}, in similarity to this artist: {artist_name}, and if available create rhymes with this phrase {rhyme}. Try your best to match the style of the artist. Unless specified, do not use slang or casual language in the lyrics generated."
 
     # Modify the prompt based on the use_slang parameter
     if use_slang:
         prompt += " You are allowed to use slang and casual language in the lyrics in this case."
+
+    # Include non-lexical vocals in the prompt if the toggle button is enabled
+    if include_non_lexical:
+        prompt += " Include some non-lexical vocals (e.g., lalalaaa) in the lyrics."
+
+        # Include a specific non-lexical vocal if provided
+        if specific_non_lexical:
+            prompt += f" Use '{specific_non_lexical}' as a non-lexical vocal."
 
     # Generate lyrics using OpenAI GPT-3
     response = openai.Completion.create(
@@ -35,12 +43,17 @@ subject = st.text_input("Subject (Optional):", "Enter the subject for this parti
 rhyme = st.text_input("Rhyme (Optional):", "Enter a particular word or phrase that you would like used")
 temperature = st.slider("Select temperature", 0.1, 1.0, 0.7, 0.1)
 use_slang = st.checkbox("Allow Slang in Lyrics", value=False, key='slang_checkbox', help='Use slang and casual language in the lyrics.')
+include_non_lexical = st.checkbox("Include Non-Lexical Vocals", value=False, key='non_lexical_checkbox', help='Include some non-lexical vocals (e.g., lalalaaa) in the lyrics.')
+
+# If the user wants to include a specific non-lexical vocal, provide a text box for it
+if include_non_lexical:
+    specific_non_lexical = st.text_input("Specific Non-Lexical Vocal (Optional):", "Enter a specific non-lexical vocal")
 
 # Generate lyrics when the user clicks the button
 if st.button("Generate Lyrics"):
     if artist_name and genre:
         # Call the generate_lyrics function
-        generated_lyric = generate_lyrics(artist_name, genre, subject, rhyme, temperature, use_slang)
+        generated_lyric = generate_lyrics(artist_name, genre, subject, rhyme, temperature, use_slang, include_non_lexical, specific_non_lexical)
 
         # Display the generated lyric
         st.success(f"Generated Lyric:\n{generated_lyric}")
