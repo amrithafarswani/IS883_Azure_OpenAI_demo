6b0b5fce10b1a60cdb58b6fc0723110ab0efb922
amrithafarswani <122059440+amrithafarswani@users.noreply.github.com>
2023-12-12 15:34:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ import random
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Function to generate lyrics
-def generate_lyrics(artist_name, genre, subject=None, rhyme=None, temperature=0.7, use_slang=False, include_non_lexical=False, specific_non_lexical=None):
+def generate_lyrics(artist_name, genre, subject=None, rhyme=None, temperature=0.7, use_slang=False, include_non_lexical=False, specific_non_lexical=None, inspiration_prompt=None):
     prompt = f"Imagine you are a songwriter. Write the lyrics to a song based on this {genre} that the author wants: {subject}, in similarity to this artist: {artist_name}, and if available create rhymes with this phrase {rhyme}. Try your best to match the style of the artist. Unless specified, do not use slang or casual language in the lyrics generated."
 
     # Modify the prompt based on the use_slang parameter
     if use_slang:
         prompt += " You are allowed to use slang and casual language in the lyrics in this case."
 
-    # Include non-lexical vocals in the prompt if the toggle button is enabled
-    if include_non_lexical:
-        prompt += " Include some non-lexical vocals (e.g., lalalaaa) in the lyrics."
+    # Generate random positions for inserting non-lexical vocals
+    positions = random.sample(range(1, 200), 5)  # Adjust the range and count as needed
 
-        # Include a specific non-lexical vocal if provided
-        if specific_non_lexical:
-            prompt += f" Use '{specific_non_lexical}' as a non-lexical vocal."
+    # Include non-lexical vocals in the prompt if the toggle button is enabled
+    if include_non_lexical and specific_non_lexical:
+        for position in positions:
+            prompt = prompt[:position] + f" {specific_non_lexical} {prompt[position:]}"  # Use the user-specified non-lexical vocal
+
+    # Include inspiration prompt if provided
+    if inspiration_prompt:
+        prompt += f" {inspiration_prompt}"
 
     # Generate lyrics using OpenAI GPT-3
     response = openai.Completion.create(
@@ -32,7 +36,6 @@ def generate_lyrics(artist_name, genre, subject=None, rhyme=None, temperature=0.
     )
 
     generated_lyric = response['choices'][0]['text']
-
     return generated_lyric
 
 # Streamlit app
@@ -48,11 +51,23 @@ use_slang = st.checkbox("Allow Slang in Lyrics", value=False, key='slang_checkbo
 include_non_lexical = st.checkbox("Include Non-Lexical Vocals", value=False, key='non_lexical_checkbox', help='Include some non-lexical vocals in the lyrics.')
 specific_non_lexical = st.text_input("Specific Non-Lexical Vocal (Optional):", "Enter a specific non-lexical vocal")
 
+# List of random inspiration prompts
+inspiration_prompts = [
+    "Write a song about the beauty of nature.",
+    "Create lyrics inspired by a memorable childhood moment.",
+    "Imagine a love story and express it in your lyrics.",
+    "Write about overcoming challenges and persevering.",
+    "Compose a song that captures the spirit of adventure."
+]
+
+# Dropdown to select an inspiration prompt
+selected_inspiration_prompt = st.selectbox("Select an Inspiration Prompt (Optional):", ["None"] + inspiration_prompts)
+
 # Generate lyrics when the user clicks the button
 if st.button("Generate Lyrics"):
     if artist_name and genre:
         # Call the generate_lyrics function
-        generated_lyric = generate_lyrics(artist_name, genre, subject, rhyme, temperature, use_slang, include_non_lexical, specific_non_lexical)
+        generated_lyric = generate_lyrics(artist_name, genre, subject, rhyme, temperature, use_slang, include_non_lexical, specific_non_lexical, selected_inspiration_prompt)
 
         # Display the generated lyric
         st.success(f"Generated Lyric:\n{generated_lyric}")
